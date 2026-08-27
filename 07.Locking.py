34e02e8dcf59ccd0f8bebc95498961d28b2b0bab
DS-73 <dhruvsaini73@gmail.com>
2026-08-27 22:25:13 +0530

    add locking example

---
+++ 07.Locking.py
@@ -0,0 +1,23 @@
+import threading
+
+counter = 0
+lock = threading.Lock()
+
+def increment():
+    global counter
+    for i in range(10**7):
+        with lock:
+            counter += 1
+
+threads: list = []
+for i in range(4):
+    x = threading.Thread(target=increment)
+    threads.append(x)
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+print(counter)
